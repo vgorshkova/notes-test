d79c24619c8325f8132777a6fa3359169041dbdb
Winter <vik.vicar@gmail.com>
2017-02-07 12:08:22 +0500

    Move actions dispatching from component to @connect.

--- src/js/components/Layout/index.jsx
+++ src/js/components/Layout/index.jsx
@@ -7,35 +7,40 @@ import * as Actions from '../../actions/actions';
 import {NOTES_NUMBER_IN_POPOVER} from '../../constants/common'
 
 
-@connect((store) => {
-    return {
-        lastNotes: store.notes.slice(-NOTES_NUMBER_IN_POPOVER).reverse(),
-        unreadNotesNumber: store.notes.filter((note) => note.unread).length,
-        isUnreadNotesPopoverOpen: store.ui.isUnreadNotesPopoverOpen,
-        isAnyNoteExist: store.notes.length
-    };
-})
+@connect(
+    state => ({
+        lastNotes: state.notes.slice(-NOTES_NUMBER_IN_POPOVER).reverse(),
+        unreadNotesNumber: state.notes.filter((note) => note.unread).length,
+        isUnreadNotesPopoverOpen: state.ui.isUnreadNotesPopoverOpen,
+        isAnyNoteExist: state.notes.length
+    }),
+
+    dispatch => ({
+        onCreateNote: (note) => {
+            dispatch(Actions.createNote(note.title, note.id, note.datetime));
+        },
+
+        onToggleUnreadNotesPopup: () => {
+            dispatch(Actions.toggleUnreadNotesPopup());
+        },
+
+        onDeleteNotes: () => {
+            dispatch(Actions.deleteNotes())
+        },
+
+        onMarkAllNotes: () => {
+            dispatch(Actions.markedAllNotesAsRead());
+        },
+
+        onGenerateNewNote: () => {
+            dispatch(Actions.addNotes());
+        }
+    })
+)
 
 export default class Layout extends React.Component {
-
-    onCreateNote = (note) => {
-        this.props.dispatch(Actions.createNote(note.title, note.id, note.datetime));
-    };
-
-    onToggleUnreadNotesPopup = () => {
-        this.props.dispatch(Actions.toggleUnreadNotesPopup());
-    };
-
-    onDeleteNotes = () => {
-        this.props.dispatch(Actions.deleteNotes())
-    };
-
-    onMarkAllNotes = () => {
-        this.props.dispatch(Actions.markedAllNotesAsRead());
-    };
-
     componentDidMount() {
-        this.props.dispatch(Actions.addNotes());
+        this.props.onGenerateNewNote();
     }
 
     render() {
@@ -45,13 +50,13 @@ export default class Layout extends React.Component {
                     notes={this.props.lastNotes}
                     unreadNotesNumber={this.props.unreadNotesNumber}
                     unreadNotesPopoverOpen={this.props.isUnreadNotesPopoverOpen}
-                    onToggleUnreadNotesPopup={this.onToggleUnreadNotesPopup}
+                    onToggleUnreadNotesPopup={this.props.onToggleUnreadNotesPopup}
                 />
-                <NoteCreator onClick={this.onCreateNote}/>
+                <NoteCreator onClick={this.props.onCreateNote}/>
                 <ManagePanel
-                    onMarkAllNotesAsRead={this.onMarkAllNotes}
-                    onDeleteNotes={this.onDeleteNotes}
-                    onToggleUnreadNotesPopup={this.onToggleUnreadNotesPopup}
+                    onMarkAllNotesAsRead={this.props.onMarkAllNotes}
+                    onDeleteNotes={this.props.onDeleteNotes}
+                    onToggleUnreadNotesPopup={this.props.onToggleUnreadNotesPopup}
                     isUnreadNotesPopoverOpen={this.props.isUnreadNotesPopoverOpen}
                     isAnyNoteExist={this.props.isAnyNoteExist}
                     unreadNotesNumber={this.props.unreadNotesNumber}
